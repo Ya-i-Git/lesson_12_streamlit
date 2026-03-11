3e21d6e14c4c9a72d5bd045beebac3ebbbf5e05c
Yaroslav <yar@MacBook-Pro-Bright.local>
2026-03-11 13:11:22 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title('Калькулятор')
+
+val_1 = st.number_input("Insert a number", key='val_1')
+val_2 = st.number_input("Insert a number", key='val_2')
+
+st.write(f'Result = {val_1 + val_2}')
